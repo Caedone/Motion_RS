0e8fb4ee31080fb88bb304f1548eee3c165e4cdf
Caedon research <CSE220000@utdallas.edu>
2024-06-25 14:27:56 -0500

    Added rounding precision

--- main.py
+++ main.py
@@ -29,8 +29,8 @@ first_file = True  # Keeps track if the first file is being read in
 for i in range(len(csv_file_list)):
     if first_file:
         df_name = globals()[f"df_{i}"]  # Retrieve the DataFrame object using globals()
-        final_row = df_name.iloc[-1].astype(float)  # Stores the final row of the first file
-        subtraction = final_row - df_name.iloc[-2].astype(float)# Subtracts the last two rows
+        final_row = df_name.iloc[-1].astype(float).round(6)  # Stores the final row of the first file
+        subtraction = final_row - df_name.iloc[-2].astype(float).round(6)# Subtracts the last two rows
 
         # Copies file to the output file
         df_name.to_csv(output_file, index=False, header=True)
@@ -49,7 +49,7 @@ for i in range(len(csv_file_list)):
 
         w = 0
         while w < row_count:
-            sum_row = (temp2 + subtraction).astype(float)  # Add final row + the subtacted rows
+            sum_row = (temp2 + subtraction).astype(float).round(6)  # Add final row + the subtacted rows
             sum_row_str = sum_row.to_frame().T.to_csv(header=False, index=False).strip()  # Convert to CSV row string and strip newlines
             rows_to_write.append(sum_row_str)  # Add to list of rows to write
             temp2 = sum_row
